16b8deed03fa9d55981f3a4b53a2a3b7bcc5e5ce
nariolf ekmud <nariolf111@gmail.com>
2024-07-04 22:31:35 +0200

    Projektdateien hinzufügen.

---
+++ ImprovedExceptionHandeling/ExceptionExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace ImprovedExceptionHandeling
+{
+    public static class ExceptionExtension
+    {
+        public static TException AddData<TException>(this TException exception, string keyword, object value) where TException : Exception
+        {
+            exception.ThrowArgumentNullExceptionIfNull();
+            keyword.ThrowArgumentExceptionIfNullOr(x => string.IsNullOrWhiteSpace(x));
+            
+            exception.Data.Add(keyword, value is null ? "Null" : JsonSerializer.Serialize(value));
+            return exception;
+        }
+
+        public static TException AddData<TException>(this TException exception, params (string keyword, object value)[] datapairs) where TException : Exception 
+        {
+            exception.ThrowArgumentNullExceptionIfNull();
+            datapairs.ThrowArgumentExceptionIfNullOr(x => !x.Any());
+            foreach (var item in datapairs)
+            {
+                exception.AddData(item.keyword,item.value);
+            }
+            return exception;
+        }
+    }
+}
---
+++ ImprovedExceptionHandeling/ThrowingExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text.Json;
+
+namespace ImprovedExceptionHandeling
+{
+    public static class ThrowingExtension
+    {
+        private static Func<ArgumentException> _error = () => new ArgumentException("Argument is Invalid. please see the Exception Data for more Detail.");
+        public static void ThrowArgumentNullExceptionIfNull<T>(this T value) where T : class
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
+        public static void ThrowArgumentExceptionIfNullOr<T>(this T value, Expression<Predicate<T>> predicate) where T : class
+        {
+            if (value is null || predicate.Compile()(value))
+            {
+                var ex = _error();
+                ex.AddData("value", JsonSerializer.Serialize(value));
+                ex.AddData("TypeOf", typeof(T));
+                ex.AddData("Expression", predicate.Body.ToString());
+                throw ex;
+            }
+        }
+        public static void ThrowArgumentExceptionIf<T>(this T value, Expression<Predicate<T>> predicate)
+        {
+            if (predicate.Compile()(value))
+            {
+                var ex = _error();
+                ex.AddData("value", JsonSerializer.Serialize(value));
+                ex.AddData("TypeOf", typeof(T).ToString());
+                ex.AddData("Expression", predicate.Body.ToString());
+                throw ex;
+            }
+        }
+
+        public static void ThrowIf<TException, TType>(this TType value, Expression<Predicate<TType>> expression, Func<TException> factory) where TException : Exception
+        {
+            if (expression.Compile()(value))
+            {
+                var ex = factory();
+                ex.AddData("value", JsonSerializer.Serialize(value));
+                ex.AddData("TypeOf", typeof(TType).ToString());
+                ex.AddData("Expression", expression.Body.ToString());
+                throw ex;
+            }
+        }
+
+    }
+}
